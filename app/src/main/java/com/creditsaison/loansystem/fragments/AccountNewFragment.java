648b79d6a989d940e01139418bef823a125af5c9
Jenny Ayala <jenny.ayala@awsys-i.com>
2019-05-10 13:10:57 +0800

    capture client photo

--- app/src/main/java/com/creditsaison/loansystem/fragments/AccountNewFragment.java
+++ app/src/main/java/com/creditsaison/loansystem/fragments/AccountNewFragment.java
@@ -7,9 +7,11 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -29,30 +31,32 @@ import com.creditsaison.loansystem.R;
 import com.creditsaison.loansystem.databinding.FragmentAccountNewBinding;
 import com.creditsaison.loansystem.viewmodel.AccountNewViewModel;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import pub.devrel.easypermissions.EasyPermissions;
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AccountNewFragment extends Fragment {
 
         // Define the pic id
-        private static final int pic_id = 123;
+        private static final int client_pic_code = 1;
 
         // Define the button and imageview type variable
-        Button btn_add_photo;
+        Button btn_add_photo, btn_gov_id;
         ImageView client_image;
         File photoFile = null;
+
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
         private FragmentAccountNewBinding binding;
         private AccountNewViewModel viewModel;
 
-    public AccountNewFragment() {
+        public AccountNewFragment() {
             // Required empty public constructor
         }
 
@@ -72,43 +76,38 @@ public class AccountNewFragment extends Fragment {
             binding.setViewModel(viewModel);
 
             btn_add_photo = (Button) binding.getRoot().findViewById(R.id.btn_add_photo);
+            btn_gov_id = (Button) binding.getRoot().findViewById(R.id.btn_add_gov_id);
             client_image = (ImageView) binding.getRoot().findViewById(R.id.client_image);
 
             btn_add_photo.setOnClickListener(new View.OnClickListener() {
-
                 @Override
                 public void onClick(View v) {
+                    if(EasyPermissions.hasPermissions(getContext(), Manifest.permission.CAMERA) &&
+                            EasyPermissions.hasPermissions(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                        Log.i("permission", "has permissions");
 
-                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
-                    } else {
                         Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
                         if (camera_intent.resolveActivity(getActivity().getPackageManager()) != null) {
                             // Create the File where the photo should go
                             try {
-
                                 photoFile = createImageFile();
                                 if (photoFile != null) {
                                     Log.i("Mayank", photoFile.getAbsolutePath());
-                                    Uri photoURI = Uri.fromFile(photoFile);
+                                    // Uri photoURI = Uri.fromFile(photoFile);
+                                    Uri photoURI = FileProvider.getUriForFile(getContext(),
+                                            "com.creditsaison.loansystem.fileprovider",
+                                            photoFile);
                                     camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                                    startActivityForResult(camera_intent, pic_id);
+                                    startActivityForResult(camera_intent, client_pic_code);
                                 }
                             } catch (Exception ex) {
                                 // Error occurred while creating the File
                                 Log.i("error", "error while creating the file");
                             }
-
-
-                        } else {
-                            Log.i("error", "error getpackagemanager");
                         }
-
-
-                        // Start the activity with camera_intent,
-                        // and request pic id
-
+                    } else {
+                        Log.i("permission", "has no camera permission");
+                        EasyPermissions.requestPermissions(getActivity(), getString(R.string.rationale_camera),1, perms);
                     }
                 }
             });
@@ -117,25 +116,33 @@ public class AccountNewFragment extends Fragment {
     }
 
 
+    public void onResume(){
+        super.onResume();
+
+        if(photoFile != null) {
+            Log.i("resume", photoFile.getAbsolutePath());
+            Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            client_image.setImageBitmap(myBitmap);
+        } else {
+            Log.i("resumed", "NULL");
+        }
+
+    }
+
+
     // This method will help to retrieve the image
     @Override
-    public void onActivityResult(int requestCode,
-                                    int resultCode,
-                                    Intent data)
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-
-        Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-        client_image.setImageBitmap(myBitmap);
-        // Match the request 'pic id with requestCode
-       // if (requestCode == pic_id) {
-
-            // BitMap is data structure of image file
-            // which stor the image in memory
-            //Bitmap photo = (Bitmap)data.getExtras().get("data");
-
-            // Set the image in imageview for display
-            //client_image.setImageBitmap(photo);
+        Log.i("activityResult", "entered activity result");
+        if(EasyPermissions.hasPermissions(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)){
+            Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            client_image.setImageBitmap(myBitmap);
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.rationale_camera),1, Manifest.permission.READ_EXTERNAL_STORAGE);
         }
+
+    }
 
 
     private File createImageFile()
@@ -157,6 +164,12 @@ public class AccountNewFragment extends Fragment {
 
         return mediaFile;
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, AccountNewFragment.this);
     }
 
 }
