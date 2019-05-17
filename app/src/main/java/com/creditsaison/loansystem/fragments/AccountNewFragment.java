81f109f643d4a41f9d4c634939ec84befe96997e
Jenny Ayala <jenny.ayala@awsys-i.com>
2019-05-17 10:45:11 +0800

    client photo portrait orientation, single id/document capture

--- app/src/main/java/com/creditsaison/loansystem/fragments/AccountNewFragment.java
+++ app/src/main/java/com/creditsaison/loansystem/fragments/AccountNewFragment.java
@@ -2,13 +2,18 @@ package com.creditsaison.loansystem.fragments;
 
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
@@ -26,6 +31,8 @@ import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.creditsaison.loansystem.R;
 import com.creditsaison.loansystem.databinding.FragmentAccountNewBinding;
@@ -41,17 +48,25 @@ import pub.devrel.easypermissions.EasyPermissions;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AccountNewFragment extends Fragment {
+public class AccountNewFragment extends Fragment implements View.OnClickListener{
 
         // Define the pic id
         private static final int client_pic_code = 1;
 
+        ConstraintLayout layout1;
+        ConstraintSet set;
+        TextView text, text1;
         // Define the button and imageview type variable
-        Button btn_add_photo, btn_gov_id;
+        Button btn_client_photo, btn_gov_id, btn_doc_photo;
         ImageView client_image;
         File photoFile = null;
+        File govIdFile = null;
+        File docFile = null;
+        Bitmap rotatedBitmap = null;
+        Spinner govSpinner, docSpinner;
 
         String[] perms = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        String govFileName, docFileName;
 
         private FragmentAccountNewBinding binding;
         private AccountNewViewModel viewModel;
@@ -75,56 +90,92 @@ public class AccountNewFragment extends Fragment {
             binding = FragmentAccountNewBinding.inflate(inflater, container, false);
             binding.setViewModel(viewModel);
 
-            btn_add_photo = (Button) binding.getRoot().findViewById(R.id.btn_add_photo);
+            btn_client_photo = (Button) binding.getRoot().findViewById(R.id.btn_add_photo);
             btn_gov_id = (Button) binding.getRoot().findViewById(R.id.btn_add_gov_id);
+            btn_doc_photo = (Button) binding.getRoot().findViewById(R.id.btn_add_doc);
             client_image = (ImageView) binding.getRoot().findViewById(R.id.client_image);
 
-            btn_add_photo.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(EasyPermissions.hasPermissions(getContext(), Manifest.permission.CAMERA) &&
-                            EasyPermissions.hasPermissions(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)){
-                        Log.i("permission", "has permissions");
+            govSpinner = (Spinner) binding.getRoot().findViewById(R.id.sp_gov_id_type);
+            docSpinner = (Spinner) binding.getRoot().findViewById(R.id.sp_doc_type);
 
-                        Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        if (camera_intent.resolveActivity(getActivity().getPackageManager()) != null) {
-                            // Create the File where the photo should go
-                            try {
-                                photoFile = createImageFile();
-                                if (photoFile != null) {
-                                    Log.i("Mayank", photoFile.getAbsolutePath());
-                                    // Uri photoURI = Uri.fromFile(photoFile);
-                                    Uri photoURI = FileProvider.getUriForFile(getContext(),
-                                            "com.creditsaison.loansystem.fileprovider",
-                                            photoFile);
-                                    camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                                    startActivityForResult(camera_intent, client_pic_code);
-                                }
-                            } catch (Exception ex) {
-                                // Error occurred while creating the File
-                                Log.i("error", "error while creating the file");
-                            }
-                        }
-                    } else {
-                        Log.i("permission", "has no camera permission");
-                        EasyPermissions.requestPermissions(getActivity(), getString(R.string.rationale_camera),1, perms);
-                    }
-                }
-            });
+            btn_client_photo.setOnClickListener(this);
+            btn_gov_id.setOnClickListener(this);
+            btn_doc_photo.setOnClickListener(this);
+
+            layout1 = (ConstraintLayout)binding.getRoot().findViewById(R.id.ConstraintLayout);
+            set = new ConstraintSet();
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(EasyPermissions.hasPermissions(getContext(), Manifest.permission.CAMERA) &&
+                EasyPermissions.hasPermissions(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+            Log.i("permission", "has permissions");
+
+            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (camera_intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                // Create the File where the photo should go
+                try {
+                    if(v == btn_client_photo) {
+                        Log.i("onClick", "client photo");
+                        photoFile = createImageFile(1);
+                        if (photoFile != null) {
+                            Log.i("Mayank", photoFile.getAbsolutePath());
+                            // Uri photoURI = Uri.fromFile(photoFile);
+                            Uri photoURI = FileProvider.getUriForFile(getContext(),
+                                    "com.creditsaison.loansystem.fileprovider",
+                                    photoFile);
+                            camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                            startActivityForResult(camera_intent, client_pic_code);
+                        }
+                    } else if(v == btn_gov_id) {
+                        Log.i("onClick", "gov id");
+                        govIdFile = createImageFile(2);
+                        if (govIdFile != null) {
+                            Log.i("Mayank", govIdFile.getAbsolutePath());
+                            Uri photoURI = FileProvider.getUriForFile(getContext(),
+                                    "com.creditsaison.loansystem.fileprovider",
+                                    govIdFile);
+                            camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                            startActivityForResult(camera_intent, 2);
+                        }
+                    } else {
+                        Log.i("onClick", "doc photo");
+                        docFile = createImageFile(3);
+                        if (docFile != null) {
+                            Log.i("Mayank", docFile.getAbsolutePath());
+                            Uri photoURI = FileProvider.getUriForFile(getContext(),
+                                    "com.creditsaison.loansystem.fileprovider",
+                                    docFile);
+                            camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                            startActivityForResult(camera_intent, 3);
+                        }
+                    }
+
+                } catch (Exception ex) {
+                    // Error occurred while creating the File
+                    Log.i("error", "error while creating the file");
+                }
+            }
+        } else {
+            Log.i("permission", "has no camera permission");
+            EasyPermissions.requestPermissions(getActivity(), getString(R.string.rationale_camera),1, perms);
+        }
     }
 
 
     public void onResume(){
         super.onResume();
 
-        if(photoFile != null) {
+        if(rotatedBitmap != null) {
             Log.i("resume", photoFile.getAbsolutePath());
-            Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-            client_image.setImageBitmap(myBitmap);
+            //Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            client_image.setImageBitmap(rotatedBitmap);
         } else {
             Log.i("resumed", "NULL");
+            client_image.setImageResource(R.drawable.ic_dp_placeholder);
         }
 
     }
@@ -136,16 +187,104 @@ public class AccountNewFragment extends Fragment {
     {
         Log.i("activityResult", "entered activity result");
         if(EasyPermissions.hasPermissions(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)){
-            Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-            client_image.setImageBitmap(myBitmap);
+            if(requestCode == 1) { //client photo
+                if(resultCode == Activity.RESULT_OK) {
+                    Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                    try {
+                        checkOrientation(myBitmap);
+                        client_image.setImageBitmap(rotatedBitmap);
+                    } catch (IOException dfg) {
+                       Log.i("rotation", "error in rotation");
+                    }
+
+                } else {
+                    photoFile = null;
+                    client_image.setImageResource(R.drawable.ic_dp_placeholder);
+                }
+            } else if(requestCode == 2 && resultCode == Activity.RESULT_OK){ //gov id
+                if(text == null) {
+                    text = new TextView(getContext());
+                    text.setId(View.generateViewId());
+
+                    if(govIdFile != null) {
+                        govFileName = govIdFile.getAbsolutePath().substring(govIdFile.getAbsolutePath().lastIndexOf("/")+1);
+                        text.setText(govFileName);
+                    }
+
+                    layout1.addView(text,0);
+                    set.clone(layout1);
+                    set.connect(text.getId(), ConstraintSet.TOP, R.id.cb_gov_id, ConstraintSet.BOTTOM, 0);
+                    set.connect(text.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
+                    set.connect(text.getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
+                    set.connect(text.getId(), ConstraintSet.BOTTOM, R.id.cb_doc, ConstraintSet.TOP, 0);
+
+                    set.applyTo(layout1);
+                } else {
+                    if(govIdFile != null) {
+                        govFileName = govIdFile.getAbsolutePath().substring(govIdFile.getAbsolutePath().lastIndexOf("/")+1);
+                        text.setText(govFileName);
+                    }
+                }
+
+            } else if(requestCode == 3 && resultCode == Activity.RESULT_OK) { //docu photo
+                if(text1 == null) {
+                    text1 = new TextView(getContext());
+                    text1.setId(View.generateViewId());
+
+                    if(docFile != null) {
+                        docFileName = docFile.getAbsolutePath().substring(docFile.getAbsolutePath().lastIndexOf("/")+1);
+                        text1.setText(docFileName);
+                    }
+
+                    layout1.addView(text1,0);
+                    set.clone(layout1);
+                    set.connect(text1.getId(), ConstraintSet.TOP, R.id.cb_doc, ConstraintSet.BOTTOM, 0);
+                    set.connect(text1.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
+                    set.connect(text1.getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
+                    set.connect(text1.getId(), ConstraintSet.BOTTOM, R.id.btn_submit, ConstraintSet.TOP, 0);
+
+                    set.applyTo(layout1);
+                } else {
+                    if(docFile != null) {
+                        docFileName = docFile.getAbsolutePath().substring(docFile.getAbsolutePath().lastIndexOf("/")+1);
+                        text1.setText(docFileName);
+                    }
+                }
+
+            }
+
         } else {
             EasyPermissions.requestPermissions(this, getString(R.string.rationale_camera),1, Manifest.permission.READ_EXTERNAL_STORAGE);
         }
 
     }
 
+    public void checkOrientation (Bitmap bitmap) throws IOException
+    {
 
-    private File createImageFile()
+        ExifInterface ei = new ExifInterface(photoFile.getAbsolutePath());
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED);
+
+        switch(orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                rotatedBitmap = rotateImage(bitmap, 90);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                rotatedBitmap = rotateImage(bitmap, 180);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                rotatedBitmap = rotateImage(bitmap, 270);
+                break;
+
+            case ExifInterface.ORIENTATION_NORMAL:
+            default:
+                rotatedBitmap = bitmap;
+        }
+    }
+    private File createImageFile(int clickedButton)
     {
         // External sdcard location
         File mediaStorageDir = Environment
@@ -159,17 +298,30 @@ public class AccountNewFragment extends Fragment {
 
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
                 Locale.getDefault()).format(new Date());
-        File mediaFile = new File(mediaStorageDir.getPath() + File.separator
+        String type = "";
+        if(clickedButton == 2) {
+            type = String.valueOf(govSpinner.getSelectedItem()).replaceAll("\\s+","_");;
+        } else if(clickedButton == 3) {
+            type = String.valueOf(docSpinner.getSelectedItem()).replaceAll("\\s+","_");;
+        }
+
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator + (type == "" ? "" : type+"_")
                 + "IMG_" + timeStamp + ".jpg");
 
         return mediaFile;
-
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, AccountNewFragment.this);
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
     }
 
 }
